c3b2e00850a6aaebed3d882c6996b4fc6fb4cf43
L3D <l3d@see-base.de>
2019-04-07 18:33:12 +0200

    Update akku warning script

--- roles/akku-warning/files/akku.sh
+++ roles/akku-warning/files/akku.sh
@@ -2,8 +2,11 @@
 
 power="$((`cat /sys/class/power_supply/BAT0/energy_now` * 100 / `cat /sys/class/power_supply/BAT0/energy_full_design`))"
 
-if (( $power < 15 && $power > 9 )); then
+if (( $power < 25 && $power > 15 )); then
     zenity --warning --title="Low Power" --text="$power percent remaining.\n\nPlease recharge soon!" --display=:0.0
+elif (( $power < 15 && $power > 9 )); then
+    zenity --warning --title="Low Power" --text="$power percent remaining.\n\nPlease recharge soon!" --display=:0.0
+    mpv /opt/low_battery.mkv -fs --volume 130 --start 00:00:18
 elif (( $power < 9 && $power > 5 )); then
     zenity --warning --title="Critical Power" --text="$power percent remaining.\n\nPlease recharge NOW!" --display=:0.0
 elif (( $power < 5 )); then
