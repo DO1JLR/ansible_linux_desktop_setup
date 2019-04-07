4a269f02a40777671b2e5de7522d4c844435f6af
L3D <l3d@see-base.de>
2019-04-07 18:42:53 +0200

    fix video

--- roles/akku-warning/files/akku.sh
+++ roles/akku-warning/files/akku.sh
@@ -6,7 +6,7 @@ if (( $power < 25 && $power > 15 )); then
     zenity --warning --title="Low Power" --text="$power percent remaining.\n\nPlease recharge soon!" --display=:0.0
 elif (( $power < 15 && $power > 9 )); then
     zenity --warning --title="Low Power" --text="$power percent remaining.\n\nPlease recharge soon!" --display=:0.0
-    mpv /opt/low_battery.mkv -fs --volume 130 --start 00:00:18
+    mpv /opt/low_battery.mkv -fs --volume 130 --start 00:00:18 --vo=tct
 elif (( $power < 9 && $power > 5 )); then
     zenity --warning --title="Critical Power" --text="$power percent remaining.\n\nPlease recharge NOW!" --display=:0.0
 elif (( $power < 5 )); then
